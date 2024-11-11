50f5ae5cd1a8bc95445d12a5a5726ee81774fcbb
hanneslsm <HannesLeismann@gmail.com>
2024-11-11 09:43:13 +0100

    via editor.scss now

--- inc/setup.php
+++ inc/setup.php
@@ -15,9 +15,6 @@ if (!function_exists('bittnerkrull25_setup')) :
 	{
 		// Make theme available for translation.
 		load_theme_textdomain('bittnerkrull25', get_template_directory() . '/languages');
-
-		// Enqueue editor styles.
-		add_editor_style('assets/css/editor-style.css');
 	}
 endif; // bittnerkrull25_setup
 add_action('after_setup_theme', 'bittnerkrull25_setup');
