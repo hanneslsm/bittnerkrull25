bd1a431a9676a340f462efc3b300f510faae216b
hanneslsm <HannesLeismann@gmail.com>
2024-11-11 16:13:16 +0100

    New Patterns

---
+++ patterns/container-1-w6633.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Title: Container 1-w6633
+ * Slug: bittnerkrull25/container-1-w6633
+ * Categories: Container
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Container 1-w6633"},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:spacer {"height":"clamp(48px, 2.5vw + 2rem, 64px)","style":{"layout":[]}} -->
+<div style="height:clamp(48px, 2.5vw + 2rem, 64px)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"66%"} -->
+<div class="wp-block-column" style="flex-basis:66%"></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":""} -->
+<div class="wp-block-column"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"clamp(48px, 2.5vw + 2rem, 64px)","style":{"layout":[]}} -->
+<div style="height:clamp(48px, 2.5vw + 2rem, 64px)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group -->
---
+++ patterns/headline-e-kicker.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Title: Headline E - Kicker
+ * Slug: bittnerkrull25/headline-e-kicker
+ * Categories: Headlines
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Heading"},"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|10"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"metadata":{"name":"Kicker"},"className":"is-style-paragraph-kicker"} -->
+<p class="is-style-paragraph-kicker"><?php esc_html_e('Kicker Text goes here', 'bittnerkrull25');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading"><?php esc_html_e('Medium heading with about six words.', 'bittnerkrull25');?></h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
