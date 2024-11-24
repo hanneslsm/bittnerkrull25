a516d0001af6785281fa3f0ce375bcd59f33b70e
hanneslsm <HannesLeismann@gmail.com>
2024-11-24 12:41:59 +0100

    imagemin

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,49 @@
-const defaultConfig = require("@wordpress/scripts/config/webpack.config");
-const path = require("path");
-const glob = require("glob");
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+const path = require( 'path' );
+const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
+const ImageMinimizerPlugin = require( 'image-minimizer-webpack-plugin' );
+const { merge } = require( 'webpack-merge' );
 
-module.exports = {
-	...defaultConfig,
-	entry: {
-	  'css/global': path.resolve(process.cwd(), "src", "global.scss"),
-	  'css/screen': path.resolve(process.cwd(), "src", "screen.scss"),
-	  'css/editor': path.resolve(process.cwd(), "src", "editor.scss"),
-	  'js/global': path.resolve(process.cwd(), "src", "global.js"),
-	},
-	output: {
-	  ...defaultConfig.output,
-	  path: path.resolve(__dirname, "build"),
-	},
-	resolve: {
-	  alias: {
-		scss: path.resolve(__dirname, 'src/scss'), // Ensures imports work correctly
-	  },
-	},
-  };
+module.exports = function ( env ) {
+	const isProduction = env && env.production;
+
+	// Define plugins array and conditionally push plugins
+	const plugins = [ ...( defaultConfig.plugins || [] ) ];
+
+	if ( isProduction ) {
+		// Copy images and minimize them only in production
+		plugins.push(
+			new CopyWebpackPlugin( {
+				patterns: [
+					{
+						from: path.resolve( __dirname, 'src/images' ),
+						to: path.resolve( __dirname, 'build/images' ),
+					},
+				],
+			} ),
+			new ImageMinimizerPlugin( {
+				minimizer: {
+					implementation: ImageMinimizerPlugin.sharpMinify,
+					options: {
+						resize: {
+							width: 2560,
+							withoutEnlargement: true,
+						},
+					},
+				},
+			} )
+		);
+	}
+
+	// Merge with defaultConfig
+	return merge( defaultConfig, {
+		entry: {
+			'css/global': path.resolve( __dirname, 'src', 'global.scss' ),
+			'css/screen': path.resolve( __dirname, 'src', 'screen.scss' ),
+			'css/editor': path.resolve( __dirname, 'src', 'editor.scss' ),
+			'js/global': path.resolve( __dirname, 'src', 'global.js' ),
+		},
+		plugins, // Use the plugins array
+		mode: isProduction ? 'production' : 'development',
+	} );
+};
