776ded309932a041abb996bd46f1e78f3572659d
hanneslsm <HannesLeismann@gmail.com>
2024-11-28 12:55:51 +0100

    Beautify code & mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,29 @@
-const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
-const path = require( 'path' );
-const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
-const ImageMinimizerPlugin = require( 'image-minimizer-webpack-plugin' );
-const { merge } = require( 'webpack-merge' );
+const defaultConfig = require("@wordpress/scripts/config/webpack.config");
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const { merge } = require("webpack-merge");
 
-module.exports = function ( env ) {
-	const isProduction = env && env.production;
-	console.log(`#### isProduction: ${isProduction} ####`);
+module.exports = function (env) {
+	const isProduction = process.env.NODE_ENV === "production";
+	const mode = isProduction ? "production" : "development";
+	console.log(`#### mode: ${mode} ####`);
 
 	// Define plugins array and conditionally push plugins
-	const plugins = [ ...( defaultConfig.plugins || [] ) ];
+	const plugins = [...(defaultConfig.plugins || [])];
 
-	if ( isProduction ) {
+	if (isProduction) {
 		// Copy images and minimize them only in production
 		plugins.push(
-			new CopyWebpackPlugin( {
+			new CopyWebpackPlugin({
 				patterns: [
 					{
-						from: path.resolve( __dirname, 'src/images' ),
-						to: path.resolve( __dirname, 'build/images' ),
+						from: path.resolve(__dirname, "src/images"),
+						to: path.resolve(__dirname, "build/images"),
 					},
 				],
-			} ),
-			new ImageMinimizerPlugin( {
+			}),
+			new ImageMinimizerPlugin({
 				minimizer: {
 					implementation: ImageMinimizerPlugin.sharpMinify,
 					options: {
@@ -38,19 +39,19 @@ module.exports = function ( env ) {
 						},
 					},
 				},
-			} )
+			}),
 		);
 	}
 
 	// Merge with defaultConfig
-	return merge( defaultConfig, {
+	return merge(defaultConfig, {
 		entry: {
-			'css/global': path.resolve( __dirname, 'src', 'global.scss' ),
-			'css/screen': path.resolve( __dirname, 'src', 'screen.scss' ),
-			'css/editor': path.resolve( __dirname, 'src', 'editor.scss' ),
-			'js/global': path.resolve( __dirname, 'src', 'global.js' ),
+			"css/global": path.resolve(__dirname, "src", "global.scss"),
+			"css/screen": path.resolve(__dirname, "src", "screen.scss"),
+			"css/editor": path.resolve(__dirname, "src", "editor.scss"),
+			"js/global": path.resolve(__dirname, "src", "global.js"),
 		},
 		plugins, // Use the plugins array
-		mode: isProduction ? 'production' : 'development',
-	} );
+		mode: mode,
+	});
 };
