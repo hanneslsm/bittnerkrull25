e9f816dc2c9d80be1f2bdff69f0910bbf28fd17c
hanneslsm <HannesLeismann@gmail.com>
2024-11-30 13:47:01 +0100

    Convert block files separately

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,30 @@ const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 const { merge } = require("webpack-merge");
-const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+const RemoveEmptyScriptsPlugin = require("webpack-remove-empty-scripts");
+const fs = require("fs");
 
 module.exports = function (env) {
 	const isProduction = process.env.NODE_ENV === "production";
 	const mode = isProduction ? "production" : "development";
 	console.log(`#### mode: ${mode} ####`);
 
+	// Dynamically get all SCSS files in the blocks folder
+	const blockStyles = fs.readdirSync(path.resolve(__dirname, "src/scss/blocks"))
+		.filter((file) => file.endsWith(".scss"))
+		.reduce((entries, file) => {
+			const name = `css/blocks/${file.replace(".scss", "")}`;
+			entries[name] = path.resolve(__dirname, "src/scss/blocks", file);
+			return entries;
+		}, {});
+
 	// Define plugins array and conditionally push plugins
 	const plugins = [
 		...(defaultConfig.plugins || []),
 		new RemoveEmptyScriptsPlugin({
 			/* ensures it runs after `*.asset.php` files are generated */
-			stage: RemoveEmptyScriptsPlugin.STAGE_AFTER_PROCESS_PLUGINS
-		})
+			stage: RemoveEmptyScriptsPlugin.STAGE_AFTER_PROCESS_PLUGINS,
+		}),
 	];
 
 	if (isProduction) {
@@ -57,9 +67,9 @@ module.exports = function (env) {
 			"css/screen": path.resolve(__dirname, "src", "screen.scss"),
 			"css/editor": path.resolve(__dirname, "src", "editor.scss"),
 			"js/global": path.resolve(__dirname, "src", "global.js"),
+			...blockStyles, // Include dynamically generated block styles
 		},
 		plugins, // Use the plugins array
 		mode: mode,
 	});
-
 };
