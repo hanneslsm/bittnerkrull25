d151ae63231cf2746ca1a5238707c2fae9e4aa82
hanneslsm <HannesLeismann@gmail.com>
2025-01-22 00:09:49 +0100

    Strip href from tags

--- functions.php
+++ functions.php
@@ -20,6 +20,9 @@ require get_template_directory() . '/inc/enqueuing.php';
 // Dashboard Widget
 require get_template_directory() . '/inc/dashboard-widget.php';
 
+// Tags
+require get_template_directory() . '/inc/tags.php';
+
 /**
  * Development only!
  */
