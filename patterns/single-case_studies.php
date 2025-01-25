dcc2e14da0f3166b59901eb75f8fbc9e12aa40e8
hanneslsm <HannesLeismann@gmail.com>
2025-01-25 17:02:30 +0100

    Save with Create Block Themes, restore slug

--- patterns/single-case_studies.php
+++ patterns/single-case_studies.php
@@ -1,12 +1,10 @@
 <?php
 /**
  * Title: single-case_studies
- * Slug: single-case_studies
+ * Slug: bittnerkrull25/single-case_studies
  * Inserter: no
  */
 ?>
-<!-- wp:template-part {"slug":"header-transparent","area":"uncategorized"} /-->
-
 <!-- wp:group {"className":"","style":{"spacing":{"margin":{"top":"-90px","bottom":"0"},"blockGap":"0","padding":{"top":"0px"}}},"layout":{"type":"default"}} -->
 <div class="wp-block-group" style="margin-top:-90px;margin-bottom:0;padding-top:0px"><!-- wp:group {"style":{"dimensions":{"minHeight":"430px"},"background":{"backgroundImage":{"url":"<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/CaseStudy-blue.png","id":3432,"source":"file","title":"CaseStudy-blue"},"backgroundSize":"cover","backgroundAttachment":"scroll"},"border":{"bottom":{"color":"var:preset|color|brand-7","width":"6px"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch","flexWrap":"nowrap","verticalAlignment":"bottom"}} -->
 <div class="wp-block-group" style="border-bottom-color:var(--wp--preset--color--brand-7);border-bottom-width:6px;min-height:430px"><!-- wp:group {"align":"wide","layout":{"type":"constrained","contentSize":"82.5rem","wideSize":""}} -->
@@ -26,7 +24,7 @@
 <div class="wp-block-group has-base-2-background-color has-background" style="border-radius:8px;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:post-featured-image /-->
 
 <!-- wp:paragraph -->
-<p><?php /* Translators: 1. is the start of a 'strong' HTML element, 2. is the end of a 'strong' HTML element */
+<p><?php /* Translators: 1. is the start of a 'strong' HTML element, 2. is the end of a 'strong' HTML element */ 
 echo sprintf( esc_html__( '%1$sEingesetzte Produkte von B+K%2$s', 'bittnerkrull25' ), '<strong>', '</strong>' ); ?></p>
 <!-- /wp:paragraph -->
 
@@ -37,17 +35,17 @@ echo sprintf( esc_html__( '%1$sEingesetzte Produkte von B+K%2$s', 'bittnerkrull2
 <!-- /wp:spacer -->
 
 <!-- wp:paragraph {"align":"left"} -->
-<p class="has-text-align-left"><?php /* Translators: 1. is the start of a 'strong' HTML element, 2. is the start of a 'strong' HTML element, 3. is the end of a 'strong' HTML element, 4. is the end of a 'strong' HTML element */
+<p class="has-text-align-left"><?php /* Translators: 1. is the start of a 'strong' HTML element, 2. is the start of a 'strong' HTML element, 3. is the end of a 'strong' HTML element, 4. is the end of a 'strong' HTML element */ 
 echo sprintf( esc_html__( '%1$s%2$sInteressieren Sie sich für eines unserer Produkte?%3$s%4$s', 'bittnerkrull25' ), '<strong>', '<strong>', '</strong>', '</strong>' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p><?php /* Translators: 1. is a 'br' HTML element, 2. is the start of a 'a' HTML element, 3. is the end of a 'a' HTML element */
+<p><?php /* Translators: 1. is a 'br' HTML element, 2. is the start of a 'a' HTML element, 3. is the end of a 'a' HTML element */ 
 echo sprintf( esc_html__( 'Rufen Sie uns einfach an unter:%1$s %2$s+49 89 458 59 50%3$s', 'bittnerkrull25' ), '<br>', '<a href="' . esc_url( 'tel:+49894585950' ) . '">', '</a>' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p><?php /* Translators: 1. is a 'br' HTML element, 2. is the start of a 'a' HTML element, 3. is the end of a 'a' HTML element */
+<p><?php /* Translators: 1. is a 'br' HTML element, 2. is the start of a 'a' HTML element, 3. is the end of a 'a' HTML element */ 
 echo sprintf( esc_html__( 'Oder schreiben Sie uns: %1$s%2$sinfo@bittner-krull.de%3$s.', 'bittnerkrull25' ), '<br>', '<a href="' . esc_url( 'mailto:info@bittner-krull.de' ) . '">', '</a>' ); ?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></aside>
