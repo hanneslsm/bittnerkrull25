9dcb2375b0d84f9cb6d1d5fd662195ade40b5422
hanneslsm <HannesLeismann@gmail.com>
2025-01-27 09:59:02 +0100

    Fix: Change title image height

--- patterns/404.php
+++ patterns/404.php
@@ -11,17 +11,16 @@
 <div class="wp-block-group" style="min-height:60dvh"><!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:group {"className":"is-style-section-base-2","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}},"border":{"radius":"8px"}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group is-style-section-base-2" style="border-radius:8px;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|base-6"}}}},"textColor":"base-6","fontSize":"xx-large"} -->
-<p class="has-text-align-center has-base-6-color has-text-color has-link-color has-xx-large-font-size"><?php /* Translators: 1. is the start of a 'strong' HTML element, 2. is the end of a 'strong' HTML element */
+<p class="has-text-align-center has-base-6-color has-text-color has-link-color has-xx-large-font-size"><?php /* Translators: 1. is the start of a 'strong' HTML element, 2. is the end of a 'strong' HTML element */ 
 echo sprintf( esc_html__( '%1$s404%2$s', 'bittnerkrull25' ), '<strong>', '</strong>' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:heading {"textAlign":"center","level":1,"style":{"typography":{"fontSize":"4rem"}}} -->
-<h1 class="wp-block-heading has-text-align-center" style="font-size:4rem"><?php esc_html_e('Page not found.', 'bittnerkrull25');?></h1>
+<h1 class="wp-block-heading has-text-align-center" style="font-size:4rem"><?php esc_html_e('Hoppla, das sollte nicht passieren!', 'bittnerkrull25');?></h1>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"align":"center"} -->
-<p class="has-text-align-center"><?php /* Translators: 1. is a 'br' HTML element */
-echo sprintf( esc_html__( 'We couldn\'t find the page you were looking for. %1$sIt may have been moved or doesn\'t exist.', 'bittnerkrull25' ), '<br>' ); ?></p>
+<p class="has-text-align-center"><?php esc_html_e('Entweder Sie haben sich vertippt oder wir haben die Seite verlegt. Aber keine Sorge, wir bringen Sie zurück auf den richtigen Weg.', 'bittnerkrull25');?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:spacer {"height":"var:preset|spacing|30"} -->
