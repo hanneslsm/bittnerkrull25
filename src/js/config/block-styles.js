38da66a4487430f6244f215731ef63760a866649
hanneslsm <HannesLeismann@gmail.com>
2025-03-12 16:17:42 +0100

    npm run format

--- src/js/config/block-styles.js
+++ src/js/config/block-styles.js
@@ -1,62 +1,63 @@
 /** Register Block Styles
  * --------------------------------------------- */
-import { registerBlockStyle } from "@wordpress/blocks";
+import { registerBlockStyle } from '@wordpress/blocks';
 
 const blockStyles = [
 	{
-		block: "core/button",
+		block: 'core/button',
 		styles: [
-			{ name: "bittnerkrull-button-brand", label: "BK Brand" },
-			{ name: "bittnerkrull-button-base", label: "BK Base" },
-			{ name: "button-ghost", label: "Ghost" }
+			{ name: 'bittnerkrull-button-brand', label: 'BK Brand' },
+			{ name: 'bittnerkrull-button-base', label: 'BK Base' },
+			{ name: 'button-ghost', label: 'Ghost' },
 		],
 	},
 	{
-		block: "core/cover",
+		block: 'core/cover',
 		styles: [
-			{ name: "bittnerkrull-cover-hover-reveal", label: "BK Hover Reveal" },
-			{ name: "cover-link", label: "Link" }
+			{
+				name: 'bittnerkrull-cover-hover-reveal',
+				label: 'BK Hover Reveal',
+			},
+			{ name: 'cover-link', label: 'Link' },
 		],
 	},
 	{
-		block: "core/details",
+		block: 'core/details',
 		styles: [
-			{ name: "details-chevron", label: "Chevron" },
-			{ name: "details-chevron-2", label: "Chevron 2" }
+			{ name: 'details-chevron', label: 'Chevron' },
+			{ name: 'details-chevron-2', label: 'Chevron 2' },
 		],
 	},
 	{
-		block: "core/gallery",
+		block: 'core/gallery',
 		styles: [
-			{ name: "bittnerkrull-frontpage-logos", label: "BK Logo Wall" },
-			{ name: "gallery-scale-effect", label: "Scale Effect" }
+			{ name: 'bittnerkrull-frontpage-logos', label: 'BK Logo Wall' },
+			{ name: 'gallery-scale-effect', label: 'Scale Effect' },
 		],
 	},
 	{
-		block: "core/list",
+		block: 'core/list',
 		styles: [
-			{ name: "list-checkmark", label: "Checkmark" },
-			{ name: "list-crossmark", label: "Crossmark" }
+			{ name: 'list-checkmark', label: 'Checkmark' },
+			{ name: 'list-crossmark', label: 'Crossmark' },
 		],
 	},
 	{
-		block: "core/paragraph",
+		block: 'core/paragraph',
 		styles: [
-			{ name: "paragraph-kicker", label: "Kicker Text" },
-			{ name: "paragraph-intro", label: "Intro Text" },
-			{ name: "paragraph-indicator", label: "Indicator" }
+			{ name: 'paragraph-kicker', label: 'Kicker Text' },
+			{ name: 'paragraph-intro', label: 'Intro Text' },
+			{ name: 'paragraph-indicator', label: 'Indicator' },
 		],
 	},
 	{
-		block: "core/navigation",
-		styles: [
-			{ name: "bittnerkrull-navigation", label: "BK Navigation" }
-		],
+		block: 'core/navigation',
+		styles: [ { name: 'bittnerkrull-navigation', label: 'BK Navigation' } ],
 	},
 ];
 
-blockStyles.forEach(({ block, styles }) => {
-	styles.forEach(({ name, label }) => {
-		wp.blocks.registerBlockStyle(block, { name, label });
-	});
-});
+blockStyles.forEach( ( { block, styles } ) => {
+	styles.forEach( ( { name, label } ) => {
+		wp.blocks.registerBlockStyle( block, { name, label } );
+	} );
+} );
